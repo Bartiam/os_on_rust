9c6dd7414a9339eef613d7da09a0ac28fa22326b
bartiam <bartiamwork@yandex.com>
2026-07-31 13:29:53 +0500

    Accesing the Page Tables. Translating Addresses. Using OffsetPageTable.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,38 @@
 
 use core::panic::PanicInfo;
 use os_on_rust::println;
+use bootloader::{BootInfo, entry_point};
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     
+    use os_on_rust::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+
+    println!("Hello World{}", "!");
     os_on_rust::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
